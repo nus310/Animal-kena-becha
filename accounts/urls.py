e8a5e95f5aa11cab85f149cfa1ea612d861d799a
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2022-05-28 03:28:35 +0600

    create a login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.accounts, name='register')
+    path('register', views.accounts, name='register'),
+    path('login/', views.login, name='accounts-login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def accounts(request):
     return render(request, 'accounts/register.html')
+
+def login(request):
+    return render(request, 'accounts/login.html')
